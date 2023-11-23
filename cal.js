2c5fb9cb5fbe11bbf78226677de3406634ff2333
debugmanoj <manojkumarwork01@gmail.com>
2023-11-23 11:56:02 +0530

    Second commit

--- cal.js
+++ cal.js
@@ -3,38 +3,60 @@ title.setAttribute("id","heading")
 title.innerText="Web Developer Task"
 document.body.append(title)
 function handleKeyPress(event) {
-    let keyCode = event.keyCode || event.which;
-
-    if (!((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105))) {
-        alert("Only numbers are allowed");
-        event.preventDefault();
+    let value=event.key
+    let check=event.keyCode
+    if((check>=65 && check<=90)||(check>=97 && check<=122) )
+    {
+        alert("Press the keyboard button ")
+    }
+    else{
+        Gots(value)
     }
 }
 let container=document.createElement("div");
 container.setAttribute("class","cal-container")
 let  mainDiv=document.createElement("div")
 mainDiv.setAttribute("class","calc")
-var keys=["","C","<--",".","X","7","8","9","4","5","6","1","2","1","+","0","00","="]
-
+var keys=["","C","<--",".","X","7","8","9","/","4","5","6","-","1","2","3","+","0","00","="]
 for(let i=0;i<keys.length;i++){
     let div = document.createElement("div");
     let button=document.createElement("button")
     
     if (i === 0) {
-        div.setAttribute("class", "cal-container");
-        
-
-    } else if (i === 1) {
         div.setAttribute("class", "Display");
+        button.innerText=keys[i]
+        button.addEventListener("keydown", handleKeyPress);
+        button.setAttribute("id","values")
+        
+        
+    } else if (i === 1) {
+        button.innerText="C"
+        div.append(button);
+        button.addEventListener("keydown", handleKeyPress);
+        button.setAttribute("id","values")
         // div.innerText = keys[i];
     } else {
-        button.innerHTML=keys[i]
+        button.innerText=keys[i]
         // div.innerText = keys[i];
-        div.addEventListener("keydown", handleKeyPress);
+        button.addEventListener("keydown", handleKeyPress);
+        button.setAttribute("id","values")
+        div.append(button);
     }
-    div.append(button)
-    container.appendChild(div);
+    
+    container.append(div);
     
 }
 mainDiv.append(container)
-document.body.append(mainDiv)
+document.body.appendChild(mainDiv)
+document.addEventListener("keydown", handleKeyPress);
+
+function  Gots(val){
+    let values=document.querySelectorAll("#values");
+    values.forEach(button => {
+
+        if(button.innerText===val){
+            
+             
+        } // Access inner text of each button
+    });
+}
